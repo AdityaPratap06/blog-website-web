ddf843c927d0b53c366ebb3e853fd1c8517b657c
lokendra3211023” <lokendra@apnaschoolapp.com>
2023-12-16 21:32:48 +0530

    build fixes

--- src/pages/[slug]/index.js
+++ src/pages/[slug]/index.js
@@ -1,11 +1,13 @@
-// import React from 'react';
+import React from 'react';
 // import { MainLayout } from '../../../app/layouts';
 // import { DefaultSidebar } from '../../../app/layouts/sidebar';
 // import DeleteInvoice from '../../../features/DeleteInvoice';
+import { Box } from '@chakra-ui/react';
 
-// export default function Page() {
-//     return <DeleteInvoice />;
-// }
+export default function Page() {
+    return(<Box></Box>)
+    // return <DeleteInvoice />;
+}
 
 // Page.getLayout = function getLayout(page) {
 //     return <MainLayout sidebar={<DefaultSidebar />}>{page}</MainLayout>;
