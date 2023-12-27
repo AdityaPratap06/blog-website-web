7727f8100cad6e486611a4059129e8967702ba6c
lokendra3211023” <lokendra@apnaschoolapp.com>
2023-12-27 23:44:35 +0530

    integrate google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import '@/styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import { initGA, logPageView } from '../Common/analytics';
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+      initGA();
+    logPageView();
+  }, []);
   return (
     <ChakraProvider>
       <Component {...pageProps} />
